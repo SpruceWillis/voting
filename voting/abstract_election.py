b3293e5a815ffac57958b2fed750bd0df09fc95f
Kevin Huang <kevin.huang2@workday.com>
2018-12-04 22:36:00 -0800

    implement abstract class and fptp voting methods

---
+++ voting/abstract_election.py
@@ -0,0 +1,11 @@
+import abc
+from abc import ABC
+
+class AbstractElection(ABC):
+    # this method should run a single round of an election
+    # it should return None if there is no winner in that round
+    # otherwise it should return an array of winner(s)
+    @staticmethod
+    @abc.abstractmethod
+    def run_election_round(ballots, votes):
+        pass
---
+++ voting/first_past_the_post.py
@@ -0,0 +1,12 @@
+from .abstract_election import AbstractElection
+from .ballot import Ballot
+
+class FirstPastThePost(AbstractElection):
+    @staticmethod
+    def run_election_round(self, ballots, votes):
+        if len(votes) > 0:
+            num_votes_max = votes.most_common(1)[0]
+            victors = {k for (k,v) in votes.items() if v == num_votes}
+            return victors
+        else:
+            return []
