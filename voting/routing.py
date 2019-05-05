eafff5e6a71e4e1b12809795683bc803973e3d90
Kevin Huang <kevinht.huang@gmail.com>
2019-05-04 23:03:10 -0700

    adjust API and template experiment

--- voting/routing.py
+++ voting/routing.py
@@ -1,29 +1,34 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 app = Flask(__name__)
 
 # TODO: implement pages
 @app.route('/')
 def landing_page():
-    return 'Hello World'
+    return render_template('landing.html')
 
-@app.route('/election/create/', methods=['GET','POST'])
+@app.route('/election/create', methods=['GET','POST'])
 def create_election():
-    if request:method == 'POST':
-        assert request.method == 'POST'
+    if request.method == 'GET':
+        assert request.method == 'GET'
+        return 'election creation page'
         # create_election(request)
-        # TODO: implement election creation
+        # TODO: implement election creation page
     else:
         pass
-        # return election_creation_page()
-        # TODO: implement election page
+        return 'election created'
+        # TODO implement election creation in DB
 
-@app.route('/vote/<election_id>/', methods=['GET', 'POST'])
+@app.route('/election/<election_id>', methods=['GET'])
+def see_election(election_id):
+    assert request.method == 'GET'
+
+@app.route('/election/<election_id>/vote', methods=['GET', 'POST'])
 def vote(election_id):
-    if request.method == 'POST':
-        assert request.method == 'POST'
-        # TODO: implement cast_vote
-        # return cast_vote(election_id, request)
+    if request.method == 'GET':
+        return "CAST VOTE HERE"
+        # display voting page
     else:
-        assert request.method == 'GET'
-        # TODO: implement ballot screen
+        assert request.method == 'POST'
+        return ""
